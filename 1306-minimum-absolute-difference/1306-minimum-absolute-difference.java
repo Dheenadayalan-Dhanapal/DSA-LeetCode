d4d309ddd9a158867e3d4e32230012df5e0987ef
Dheenadayalan Dhanapal <96131098+Dheenadayalan-Dhanapal@users.noreply.github.com>
2024-10-30 16:05:43 +0530

    Time: 20 ms (23.36%), Space: 55.4 MB (95.94%) - LeetHub

--- 1306-minimum-absolute-difference/1306-minimum-absolute-difference.java
+++ 1306-minimum-absolute-difference/1306-minimum-absolute-difference.java
@@ -5,11 +5,11 @@ class Solution {
         int n = arr.length;
         List<List<Integer>> res = new ArrayList<>();
         for(int i = 0; i < n-1; i++){
-            min = Math.min(min, Math.abs(arr[i+1] - arr[i]));
+            min = Math.min(min, Math.abs(arr[i] - arr[i+1]));
         }
         for(int i = 0; i < n-1; i++){
             List<Integer> tmp = new ArrayList<Integer>();
-            if((arr[i + 1] - arr[i]) == min){
+            if(Math.abs(arr[i] - arr[i+1]) == min){
                 tmp.add(arr[i]);
                 tmp.add(arr[i+1]);
                 res.add(tmp);
