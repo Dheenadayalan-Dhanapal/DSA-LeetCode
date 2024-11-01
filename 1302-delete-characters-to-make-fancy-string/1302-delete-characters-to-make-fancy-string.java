af6bb51a701618c922fa5a0dac32c26408837aa3
Dheenadayalan Dhanapal <96131098+Dheenadayalan-Dhanapal@users.noreply.github.com>
2024-11-01 22:53:32 +0530

    Time: 33 ms (59.3%), Space: 45.5 MB (63.92%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
@@ -1,25 +1,25 @@
 class Solution {
     public String makeFancyString(String s) {
-        char[] str = s.toCharArray();
+        // char[] str = s.toCharArray();
+        StringBuilder ans = new StringBuilder();
         int count = 1;
-        List<Character> chr = new ArrayList<Character>();
-        chr.add(str[0]);
-        for(int i = 1; i < str.length; i++){
-            if(str[i] == str[i-1]){
+        // List<Character> chr = new ArrayList<Character>();
+        // chr.add(str[0]);
+        ans.append(s.charAt(0));
+        for(int i = 1; i < s.length(); i++){
+            if(s.charAt(i) == s.charAt(i-1)){
                 count++;
                 if(count < 3){
-                    chr.add(str[i]);
+                    // chr.add(str[i]);
+                    ans.append(s.charAt(i));
                 }
             }
             else{
-                chr.add(str[i]);
+                // chr.add(str[i]);
+                ans.append(s.charAt(i));
                 count = 1;
             }
         }
-        s = chr.stream().map(String::valueOf).collect(Collectors.joining());
-        // for(char ch : chr){
-        //     s += ch;
-        // }
-        return s;
+        return ans.toString();
     }
 }
