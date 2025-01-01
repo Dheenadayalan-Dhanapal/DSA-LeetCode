822ad405ed30d9dd20f84d589990fe3a75257fb8
Dheenadayalan Dhanapal <96131098+Dheenadayalan-Dhanapal@users.noreply.github.com>
2025-01-01 07:53:58 +0530

    Time: 3 ms (98.13%), Space: 60.2 MB (25.43%) - LeetHub

--- 1206-corporate-flight-bookings/1206-corporate-flight-bookings.java
+++ 1206-corporate-flight-bookings/1206-corporate-flight-bookings.java
@@ -1,13 +1,20 @@
 class Solution {
     public int[] corpFlightBookings(int[][] bookings, int n) {
-        int[] output = new int[n];
-        for(int i = 0; i < bookings.length; i++){
-            int startIndex = bookings[i][0];
-            int endIndex = bookings[i][1];
-            for(int j = startIndex; j <= endIndex; j++){
-                output[j - 1] = output[j - 1] + bookings[i][2];
+        int[] res = new int[n];
+        for(int[] booking: bookings){
+            int start = booking[0] - 1;
+            int end   = booking[1];
+            int val   = booking[2];
+            res[start] = res[start] + val;
+
+            if(end < n){
+                res[end] = res[end] - val;
             }
         }
-        return output;
+
+        for(int i = 1; i < n; i++){
+            res[i] += res[i - 1];
+        }
+        return res;
     }
 }
