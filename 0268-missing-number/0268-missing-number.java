d4e5090c62fe655b124fd1b570f742445dd2a04a
Dheenadayalan Dhanapal <96131098+Dheenadayalan-Dhanapal@users.noreply.github.com>
2025-03-25 16:20:44 +0530

    Time: 0 ms (100%), Space: 45.2 MB (70.75%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -3,16 +3,12 @@ class Solution {
         int N = nums.length;
         int xor1 = 0;
         int xor2 = 0;
-        int res = 0;
         for(int i = 0; i < N + 1; i++){
             xor1 ^= i;
         }
         for(int i = 0; i < N; i++){
             xor2 ^= nums[i];
         }
-        for(int i = 0; i < N; i++){
-            res = xor1 ^ xor2;
-        }
-        return res;
+        return xor1 ^ xor2;
     }
 }
