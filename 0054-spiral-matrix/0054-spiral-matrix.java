d7fc850f67393e9e14a520e356ff3e41cca01280
Dheenadayalan Dhanapal <96131098+Dheenadayalan-Dhanapal@users.noreply.github.com>
2025-04-07 12:20:10 +0530

    Time: 0 ms (100%), Space: 41.6 MB (42.6%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,39 +1,27 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> lst = new ArrayList<>();
-        if (matrix == null || matrix.length == 0) {
-            return lst;
-        }
-        
         int top = 0;
         int bottom = matrix.length - 1;
         int left = 0;
         int right = matrix[0].length - 1;
-        
-        while (top <= bottom && left <= right) {
-            // Traverse from left to right
-            for (int j = left; j <= right; j++) {
-                lst.add(matrix[top][j]);
+        List<Integer> lst = new ArrayList<>();
+        while(top <= bottom && left <= right){
+            for(int i = left; i <= right; i++){
+                lst.add(matrix[top][i]);
             }
             top++;
-            
-            // Traverse from top to bottom
-            for (int i = top; i <= bottom; i++) {
+            for(int i = top; i <= bottom; i++){
                 lst.add(matrix[i][right]);
             }
             right--;
-            
-            // Check if there is a bottom row to traverse
-            if (top <= bottom) {
-                for (int j = right; j >= left; j--) {
-                    lst.add(matrix[bottom][j]);
+            if(top <= bottom){
+                for(int i = right; i >= left; i--){
+                    lst.add(matrix[bottom][i]);
                 }
                 bottom--;
             }
-            
-            // Check if there is a left column to traverse
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
+            if(left <= right){
+                for(int i = bottom; i >= top; i--){
                     lst.add(matrix[i][left]);
                 }
                 left++;
