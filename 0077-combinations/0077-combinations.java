d41094e06b5e9adaf2640592c186962fc736b947
Dheenadayalan Dhanapal <96131098+Dheenadayalan-Dhanapal@users.noreply.github.com>
2026-03-25 07:57:03 +0530

    Time: 18 ms (82.78%), Space: 97.8 MB (88.88%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,28 +1,22 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> result = new ArrayList<>();
-        List<Integer> currentSubset = new ArrayList<>();
-        if(n < 1){
-            return result;
-        }
         int[] arr = new int[n];
-        for(int i = 1; i <= n; i++){
+        for (int i = 1; i <= n; i++) {
             arr[i - 1] = i;
         }
-        Backtrack(n, k, 0, result, currentSubset, arr);
+        Backtracking(n, k, 0, new ArrayList<>(), result, arr);
         return result;
     }
-    public void Backtrack(int n, int k, int idx, List<List<Integer>> result, List<Integer> currentSubset, int[] arr){
-        if(currentSubset.size() == k){
+
+    public void Backtracking(int n, int k, int idx, List<Integer> currentSubset, List<List<Integer>> result, int[] arr) {
+        if (currentSubset.size() == k) {
             result.add(new ArrayList<>(currentSubset));
             return;
         }
-        for(int i = idx; i < n; i++){
-            // Include
+        for (int i = idx; i < arr.length; i++) {
             currentSubset.add(arr[i]);
-            // Backtracking
-            Backtrack(n, k, i + 1, result, currentSubset, arr);
-            // Exclude
+            Backtracking(n, k, i + 1, currentSubset, result, arr);
             currentSubset.remove(currentSubset.size() - 1);
         }
     }
